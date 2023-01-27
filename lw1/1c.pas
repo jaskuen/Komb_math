0cfe2b93d7ed30b255a3c203655584097ebcc9db
InfoGuest <107575091+InfoGuest@users.noreply.github.com>
2023-01-27 13:31:01 +0300

    lw 1.3

---
+++ lw1/1c.pas
@@ -0,0 +1,34 @@
+﻿PROGRAM RandomPerm(INPUT, OUTPUT);
+USES
+  CRT;
+CONST
+  N = 10;
+VAR
+  A: ARRAY [1 .. N] OF INTEGER;
+  I, J: INTEGER;
+  
+PROCEDURE SWAP(VAR A, B: INTEGER);
+VAR
+  T: INTEGER;
+BEGIN
+  T := A;
+  A := B;
+  B := T
+END;
+
+BEGIN
+  FOR I := 1 TO N
+  DO
+    A[I] := I;
+  FOR I := N DOWNTO 1
+  DO
+    BEGIN
+      J := RANDOM(N) + 1;
+      SWAP(A[J], A[I])
+    END;
+  FOR I := 1 TO N
+  DO
+    WRITE(A[I]);
+  WRITELN
+END.
+    
